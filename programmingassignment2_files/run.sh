3f858e8f2b93c974a9ad2dcaa811a7f1e7a7ff59
Nilesh Sah <nilesh.sah13@gmail.com>
2020-02-25 21:09:46 +0100

    Add run bash script to init routers

---
+++ programmingassignment2_files/run.sh
@@ -0,0 +1,10 @@
+make ls &&
+./ls_router 0 example_topology/testinitcosts0 logs/out0.log 2>> /tmp/router.log &
+./ls_router 1 example_topology/testinitcosts1 logs/out1.log 2>> /tmp/router.log &
+./ls_router 2 example_topology/testinitcosts2 logs/out2.log 2>> /tmp/router.log &
+./ls_router 3 example_topology/testinitcosts3 logs/out3.log 2>> /tmp/router.log &
+./ls_router 4 example_topology/testinitcosts4 logs/out4.log 2>> /tmp/router.log &
+./ls_router 5 example_topology/testinitcosts5 logs/out5.log 2>> /tmp/router.log &
+./ls_router 6 example_topology/testinitcosts6 logs/out6.log 2>> /tmp/router.log &
+./ls_router 7 example_topology/testinitcosts7 logs/out7.log 2>> /tmp/router.log &
+./ls_router 255 example_topology/testinitcosts255 logs/out255.log 2>> /tmp/router.log &
